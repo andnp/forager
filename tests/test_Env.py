027aa913457fb059629b011bb758d65955014f27
andy <andnpatterson@gmail.com>
2023-06-01 21:56:00 -0600

    test: add majority of unit test coverage

--- tests/test_Env.py
+++ tests/test_Env.py
@@ -1,8 +1,10 @@
-import unittest
 import pickle
+import unittest
+import numpy as np
 from forager.Env import ForagerEnv
 from forager.config import ForagerConfig
 from forager.objects.Flower import Flower
+from forager.objects.Wall import Wall
 
 class TestForagerEnv(unittest.TestCase):
     def test_pickleable(self):
@@ -49,3 +51,103 @@ class TestForagerEnv(unittest.TestCase):
         env = ForagerEnv(config)
 
         self.assertEqual(len(env._object_configs), int(100 * 0.1))
+
+    def test_basic_movement(self):
+        config = ForagerConfig(
+            size=7,
+            objects=[
+                Wall((3, 4)),
+                Wall((3, 5)),
+            ],
+        )
+        env = ForagerEnv(config)
+
+        self.assertEqual(env._state, (3, 3))
+
+        # stays still when bumping into a wall
+        _ = env.step(0)
+        self.assertEqual(env._state, (3, 3))
+
+        _ = env.step(1)
+        self.assertEqual(env._state, (4, 3))
+
+        _ = env.step(3)
+        self.assertEqual(env._state, (3, 3))
+
+        _ = env.step(2)
+        self.assertEqual(env._state, (3, 2))
+
+    def test_vision(self):
+        config = ForagerConfig(
+            size=7,
+            objects=[
+                Wall((3, 4)),
+                Wall((3, 5)),
+                Wall((0, 2)),
+            ],
+            aperture=3,
+        )
+        env = ForagerEnv(config)
+        self.assertEqual(env._state, (3, 3))
+
+        x, _ = env.step(0)
+        expected = np.array([
+            [0, 1, 0],
+            [0, 0, 0],
+            [0, 0, 0],
+        ], dtype=np.bool_)
+
+        self.assertEqual(env._state, (3, 3))
+        self.assertTrue(np.allclose(x[:, :, 1], expected))
+
+        env.step(1)
+        x, _ = env.step(0)
+        expected = np.array([
+            [1, 0, 0],
+            [1, 0, 0],
+            [0, 0, 0],
+        ], dtype=np.bool_)
+
+        self.assertEqual(env._state, (4, 4))
+        self.assertTrue(np.allclose(x[:, :, 1], expected))
+
+        env.step(1) # (5, 4)
+        x, _ = env.step(1) # (6, 4)
+        expected_0 = np.array([
+            [0, 0, 1],
+            [0, 0, 1],
+            [0, 0, 1],
+        ], dtype=np.bool_)
+
+        self.assertEqual(env._state, (6, 4))
+        self.assertTrue(np.allclose(x[:, :, 0], expected_0))
+
+    def test_respawn(self):
+        flower = Flower()
+        flower.freq = None
+        flower.location = (3, 4)
+
+        config = ForagerConfig(
+            size=7,
+            objects=[flower],
+            aperture=3,
+        )
+        env = ForagerEnv(config, seed=1)
+        self.assertEqual(len(env._to_respawn), 0)
+
+        _, r = env.step(0)
+        self.assertEqual(r, 1)
+        self.assertEqual(len(env._object_configs), 0)
+        self.assertEqual(len(env._to_respawn), 1)
+        self.assertEqual(env._clock, 1)
+
+        for _ in range(50):
+            env.step(0)
+
+        self.assertEqual(len(env._to_respawn), 1)
+        self.assertEqual(env._clock, 51)
+
+        env.step(0)
+        self.assertEqual(env._clock, 52)
+        self.assertEqual(len(env._object_configs), 1)
+        self.assertEqual(len(env._to_respawn), 0)
