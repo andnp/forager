aba885cdf8d59c662055842b9446ba9e1ac54e9f
andy <andnpatterson@gmail.com>
2023-06-01 21:53:59 -0600

    feat: add placeable wall element

--- forager/objects/Wall.py
+++ forager/objects/Wall.py
@@ -1,13 +1,25 @@
 import numpy as np
 from forager.config import ForagerObject
+from forager.interface import Coords
 
-class Wall(ForagerObject):
+class WallSpawn(ForagerObject):
     def __init__(self):
         super().__init__(name='wall')
 
         self.blocking = True
         self.collectable = False
-        self.freq = 0.3
+        self.freq = 0.2
+
+    def reward(self, rng: np.random.Generator, clock: int) -> float:
+        return 0
+
+class Wall(ForagerObject):
+    def __init__(self, loc: Coords):
+        super().__init__(name='wall')
+
+        self.blocking = True
+        self.collectable = False
+        self.location = loc
 
     def reward(self, rng: np.random.Generator, clock: int) -> float:
         return 0
