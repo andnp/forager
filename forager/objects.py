d11428b1f50c7722b1d4c40392552403c764c40b
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-02-05 23:19:01 -0700

    Add Truffle and Oyster objects

--- forager/objects.py
+++ forager/objects.py
@@ -60,3 +60,37 @@ class Thorns(ForagerObject):
 
     def reward(self, rng: np.random.Generator, clock: int) -> float:
         return -1
+
+
+class Truffle(ForagerObject):
+    def __init__(self, loc: Coords | None = None):
+        super().__init__(name="truffle")
+
+        self.blocking = False
+        self.collectable = True
+        self.target_location = loc
+        self.color = np.array((189, 172, 163), dtype=np.uint8)
+
+    def regen_delay(self, rng: np.random.Generator, clock: int) -> int | None:
+        self.target_location = self.current_location
+        return 100
+
+    def reward(self, rng: np.random.Generator, clock: int) -> float:
+        return 10
+
+
+class Oyster(ForagerObject):
+    def __init__(self, loc: Coords | None = None):
+        super().__init__(name="oyster")
+
+        self.blocking = False
+        self.collectable = True
+        self.target_location = loc
+        self.color = np.array((196, 198, 200), dtype=np.uint8)
+
+    def regen_delay(self, rng: np.random.Generator, clock: int) -> int | None:
+        self.target_location = self.current_location
+        return 10
+
+    def reward(self, rng: np.random.Generator, clock: int) -> float:
+        return 1
